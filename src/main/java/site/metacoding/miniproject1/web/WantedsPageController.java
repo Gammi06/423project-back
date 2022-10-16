e1011f080177f0b422709342171d6b33a5a596d8
SINGIN <104334760+Gammi06@users.noreply.github.com>
2022-10-16 12:09:14 +0900

    공고 상세보기service, dao작성 완료 및 컨트롤러 수정 완료

--- src/main/java/site/metacoding/miniproject1/web/WantedsPageController.java
+++ src/main/java/site/metacoding/miniproject1/web/WantedsPageController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject1.service.WantedsService;
 import site.metacoding.miniproject1.web.dto.response.codes.AllCodesDto;
 import site.metacoding.miniproject1.web.dto.response.wanteds.PagingWantedsListDto;
+import site.metacoding.miniproject1.web.dto.response.wanteds.WantedDetailDto;
 import site.metacoding.miniproject1.web.dto.response.wanteds.WantedsListDto;
 
 @RequiredArgsConstructor
@@ -21,18 +22,20 @@ public class WantedsPageController {
 	public String wantedsPage(Model model, Integer userId, Integer page, Integer state) {
 		// 세션에서 유저의 id 혹은 user_id 가져와서 입력하기
 		// 로그인체크해서 할필요 없으면 넘겨버리기
-		List<WantedsListDto> wantedsLikeList = wantedsService.findByLike(1);
+		List<WantedsListDto> wantedsLikeListPS = wantedsService.findByLike(1);
 		PagingWantedsListDto pagingWantedsListPS = wantedsService.pagingWantedsList(page, state);		
 		AllCodesDto allCodesPS = wantedsService.findAllCodes();
 		
-		model.addAttribute("wantedsLikeList", wantedsLikeList);
+		model.addAttribute("wantedsLikeList", wantedsLikeListPS);
 		model.addAttribute("pagingWantedsList", pagingWantedsListPS);
 		model.addAttribute("allCodes", allCodesPS);
 		return "wanteds/wanted";
 	}
 	
 	@GetMapping("/wanteds/{id}")
-	public String wantedsDetailPage(Integer id) {
+	public String wantedsDetailPage(Model model, Integer id) {
+		WantedDetailDto wantedDetailDtoPS = wantedsService.findByIdToDetail(id);
+		model.addAttribute("wantedDetail", wantedDetailDtoPS);
 		return "wanteds/wanted-detail";
 	}
 }
