3e49f03fb2dbf953453d33800ac00337b77b772a
SINGIN <104334760+Gammi06@users.noreply.github.com>
2022-10-17 12:09:03 +0900

    Controller로 해당 jsp 연결 완료

--- src/main/java/site/metacoding/miniproject1/web/CompanyPageController.java
+++ src/main/java/site/metacoding/miniproject1/web/CompanyPageController.java
@@ -1,6 +1,7 @@
 package site.metacoding.miniproject1.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject1.service.CompanysService;
@@ -9,4 +10,9 @@ import site.metacoding.miniproject1.service.CompanysService;
 @Controller
 public class CompanyPageController {
 	private final CompanysService companysService;
+	
+	@GetMapping("/company/detail")
+	public String detailPage() {
+		return "companys/company-detail";
+	}
 }
