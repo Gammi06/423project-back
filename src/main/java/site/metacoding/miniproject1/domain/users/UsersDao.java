c63fed018074742680fc921569332d2e2319260b
SungYu05 <114338362+SungYu05@users.noreply.github.com>
2022-10-18 14:23:32 +0900

    Loginpage topic (#4)
    * loginpage 시작
    
    * 로그인을 위한 DAO 생성
    
    * 로그인을 위한UsersLoginReqDto 생성
    
    * 서비스 완료
    
    * jsp, css 완료
    
    * 컨트롤러 완료
    
    * 자바스크립트 완료
    
    * 로그아웃 구현 완료
    
    * loginpage 구현 완료
    
    Co-authored-by: SINGIN <104334760+Gammi06@users.noreply.github.com>

--- src/main/java/site/metacoding/miniproject1/domain/users/UsersDao.java
+++ src/main/java/site/metacoding/miniproject1/domain/users/UsersDao.java
@@ -7,6 +7,9 @@ public interface UsersDao {
 	public void insert(Users users);
 	public List<Users> findAll();
 	public Users findById(Integer id);
-	public void update(Integer id, Users users);
+	public void update(Users users);
 	public void deleteById(Integer id);
+	
+	// 로그인을 위한 Dao
+	public Users findByUserId(String userId); 
 }
